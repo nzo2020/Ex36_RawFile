26155689f63aa4f76432c2d562f7ab3a751e7ace
nzo2020 <nz2020@bs.amalnet.k12.il>
2025-03-08 21:48:15 +0200

    ex36_rawfile

--- app/src/main/java/com/example/ex36_rawfile/MainActivity.java
+++ app/src/main/java/com/example/ex36_rawfile/MainActivity.java
@@ -8,8 +8,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
