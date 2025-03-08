50bb90c1c2292632c280ff467f4d4bbc672ddc0f
nzo2020 <nz2020@bs.amalnet.k12.il>
2025-03-08 14:26:34 +0200

    Ex36_RawFile

---
+++ app/src/main/java/com/example/ex36_rawfile/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.ex36_rawfile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvText;
+    private EditText etText;
+    private final String FILENAME = "rawtest.txt";
+    private int resourceId;
+    private String fileName = FILENAME.substring(0, FILENAME.length() - 4);
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvText = findViewById(R.id.tvText);
+        etText = findViewById(R.id.etText);
+
+        resourceId = this.getResources().getIdentifier(fileName, "raw", this.getPackageName());
+
+    }
+
+    public void raw_file_click(View view){
+        try {
+            InputStream iS = this.getResources().openRawResource(resourceId);
+            InputStreamReader iSR = new InputStreamReader(iS);
+            BufferedReader bR = new BufferedReader(iSR);
+            StringBuilder sB = new StringBuilder();
+            String line = bR.readLine();
+            while (line != null) {
+                sB.append(line+'\n');
+                line = bR.readLine();
+            }
+            bR.close();
+            iSR.close();
+            iS.close();
+            tvText.setText(sB.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void text_click(View view){
+        tvText.setText(etText.getText().toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.creditsmain, menu);
+        return true;
+    }
+
+    /**
+     * Handles menu item selections, navigating to the credits screen if selected.
+     *
+     * @param item The selected menu item.
+     * @return True if the selection was handled.
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menuCredits) {
+            Intent intent = new Intent(this, mainCredits.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
