c54259fde4422d0bd04277ab77181893ea06c433
nzo2020 <nz2020@bs.amalnet.k12.il>
2025-03-08 21:38:54 +0200

    ex36_rawfile

--- app/src/main/java/com/example/ex36_rawfile/MainActivity.java
+++ app/src/main/java/com/example/ex36_rawfile/MainActivity.java
@@ -7,14 +7,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
